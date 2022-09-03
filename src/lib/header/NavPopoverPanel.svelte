241d815c52c10021f9e87dd6394b6365619f7673
Jordan Calhoun <jordancalhoun@gmail.com>
2022-09-03 15:47:35 -0400

    Mobile navigation updates

--- src/lib/header/NavPopoverPanel.svelte
+++ src/lib/header/NavPopoverPanel.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-    import { PopoverPanel } from "@rgossiaux/svelte-headlessui";
+    import { PopoverPanel, PopoverButton } from "@rgossiaux/svelte-headlessui";
     import { fade, slide  } from 'svelte/transition';
     import { quintOut } from 'svelte/easing';
+    import { XMark } from 'svelte-heros-v2';
 
     type navLink = {
         name: string;
         href: string;
+        src?: string;
     }
 
     const navLinks: navLink[] = [
@@ -26,24 +28,71 @@
             href: "#"
         },
     ]
+
+    const socialLinks: navLink[] = [
+        {
+            name: "Github",
+            href:"https://github.com/jordancalhoun",
+            src: "/images/social-icons/Github.svg"
+        },
+        {
+            name: "Twitter",
+            href:"https://twitter.com/jordancalhoun",
+            src: "/images/social-icons/Twitter.svg"
+        },
+        {
+            name: "Spotify",
+            href:"https://open.spotify.com/user/jordancalhoun?si=6d2a862877bd4334",
+            src: "/images/social-icons/Spotify.svg"
+        },
+        {
+            name: "YouTube",
+            href:"https://www.youtube.com/channel/UChHHysDsAnjZ_rnzXbwI59A",
+            src: "/images/social-icons/YouTube.svg"
+        },
+        {
+            name: "Instagram",
+            href:"https://instagram.com/mortiseco",
+            src: "/images/social-icons/Instagram.svg"
+        },
+    ]
 </script>
 
 <PopoverPanel class="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden">
-    <div class="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5" transition:slide="{{delay: 250, duration: 300, easing: quintOut }}">
-        <div class="p-6">
+    <div class="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5" transition:slide="{{delay: 100, duration: 300, easing: quintOut }}">
+        <div class="px-6 pt-4 pb-5">
             <div class="flex items-center justify-between">
                 <a href="/">
-                    <span class="font-bold text-2xl text-black">JORDAN CAHOUN</span>
+                    <span class="font-bold text-md text-black">JORDAN CAHOUN</span>
                 </a>
+                <div class="-mr-2">
+                    <PopoverButton class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <span class="sr-only">Close menu</span>
+                        <XMark class="h-6 w-6" aria-hidden="true" />
+                    </PopoverButton>
+                </div>
             </div>
+           
 
-            <ul class="font-bold text-xls text-black">
+            <ul class="font-bold text-xls text-black py-5">
                 {#each navLinks as link}
-                    <a href={link.href}>
-                        <li class="py-1">{link.name}</li>
+                    <a href={link.href} class="hover:underline hover:text-violet-600">
+                        <li class="py-2 text-2xl ">{link.name}</li>
                     </a>
                 {/each}
             </ul>
+            <div class="border-t-2 border-t-slate-200 pt-5">
+                <ul >
+                    {#each socialLinks as link}
+                    <a href={link.href}>
+                        <li class="w-auto h-auto inline-block px-3">
+                            <img src={link.src} alt={link.name}/>
+                            <!-- TODO: Add static dimensions to images -->
+                        </li>
+                    </a>
+                    {/each}
+                </ul>
+            </div>
         </div>
     </div>
 </PopoverPanel>
