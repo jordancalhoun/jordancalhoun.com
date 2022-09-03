7f75dfd91900eca4dbdaf6a44056ff0185a83bc7
Jordan Calhoun <jordancalhoun@gmail.com>
2022-09-03 06:35:09 -0400

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "../app.scss";
+</script>
+
+<!-- #TODO: Importing Inter font from remote, is this the best soltuion? -->
+<svelte:head>
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+</svelte:head>
+  
+<slot />
