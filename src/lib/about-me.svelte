e9642c031d4edda3586f60ea6baa44851411b28e
Jordan Calhoun <jordancalhoun@gmail.com>
2022-09-05 13:20:40 -0400

    add margin to about me

--- src/lib/about-me.svelte
+++ src/lib/about-me.svelte
@@ -1,8 +1,4 @@
 <style>
-    #shape-wrapper {
-
-    }
-
     #about-me-top {
         background-image: url('/images/about-me-top.svg');
         height: 100px;
@@ -19,7 +15,7 @@
         background-repeat: no-repeat;
     }
 </style>
-<div id="about-me-top" class="sm:hidden"/>
+<div id="about-me-top" class="sm:hidden mt-20"/>
 <div id="shape-wrapper" class="bg-violet-500 p-5">
     <img src="/images/temp/profile-picture.png" alt="Profile of Jordan Calhoun" class="float-right pr-8" />
     <h2 class="text-4xl font-bold text-violet-50 mb-10">About Me</h2>
@@ -31,4 +27,4 @@
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officiis suscipit vero, maiores cumque temporibus quae vel aliquam quo, eos facilis, adipisci laborum consectetur? Id repellendus vitae illum molestias cumque.
     </p>
 </div>
-<div id="about-me-bottom" />
+<div id="about-me-bottom" class="mb-20" />
