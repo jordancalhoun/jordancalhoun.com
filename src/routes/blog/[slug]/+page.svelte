de9df2c2901f2cbd5458f78c28f7c03f7bde4aab
Jordan Calhoun <jordancalhoun@gmail.com>
2022-09-08 15:51:34 -0400

    Add dynamic post basics

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import type { ComponentType } from "svelte";
+
+    
+    // #TODO: Create a type for the data object that is passed.
+    type Data = {
+        title: string;
+        date: string;
+        Content: ComponentType;
+    }
+    export let data: Data;
+    const { title, date, Content } = data
+
+</script>
+
+<article class="prose prose-img:rounded-lg m-5 lg:prose-xl mx-auto mt-10">
+    <h1>{ title }</h1>
+    <p>Published: {date}</p>
+    <Content />
+</article>
