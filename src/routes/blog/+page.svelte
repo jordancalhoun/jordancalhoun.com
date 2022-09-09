7ce7cf667881fa0c223a2c49a8b0c76691ecd78d
Jordan Calhoun <jordancalhoun@gmail.com>
2022-09-09 14:12:15 -0400

    Add super basic blog.  Index page/blog view page

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let data: any;
+</script>
+<div class="mx-auto max-w-5xl p-10">        
+    <ul class="mt-10">
+        {#each data.posts as post}
+        <li class="p-5 font-bold ">
+            <h2 class="text-2xl">
+            <a href={post.path} class="hover:text-violet-600">
+                {post.meta.title}
+            </a>
+        </h2>
+        <span class="text-gray-400">Published {post.meta.date}</span>
+    </li>
+    {/each}
+    </ul>
+</div>
---
+++ src/routes/blog/posts/about-me.md
@@ -0,0 +1,5 @@
+---
+title: Welcome to the blog
+date: "2022-09-09"
+---
+Hi im Jordan and i am a full stack dev based in a cornfield in PA
