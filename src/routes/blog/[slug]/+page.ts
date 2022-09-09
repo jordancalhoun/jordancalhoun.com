2595cc1e1a81b38c62d020357ec044a0b6d84de5
Jordan Calhoun <jordancalhoun@gmail.com>
2022-09-09 06:24:10 -0400

    Fix Vite error for dymanic loading

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from './$types'
 
 export const load: PageLoad = async ({ params }) => {
     //  TODO: Implement a try catch block for error handling.
-    const post = await import(`./${params.slug}.md`)
+    const post = await import(`../posts/${params.slug}.md`)
     const { title, date }  = post.metadata
     const Content = post.default
 
