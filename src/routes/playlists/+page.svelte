7fb578205d26418707862de3903d77caa5a6c9f0
Jordan Calhoun <jordancalhoun@gmail.com>
2022-10-05 21:27:25 -0400

    Clean up playlist page

--- src/routes/playlists/+page.svelte
+++ src/routes/playlists/+page.svelte
@@ -1,6 +1,6 @@
 <div class="px-7 my-7 lg:flex md:px-20 mx-auto max-w-7xl">
   <div
-    class="bg-green-300 p-8 grid grid-cols-[275px_minmax(0px,_1fr)_0px] items-center selection:bg-green-100 selection:text-green-900"
+    class="bg-green-300 p-8 pl-0 grid grid-cols-[275px_minmax(0px,_1fr)_0px] items-center selection:bg-green-100 selection:text-green-900 rounded-md"
   >
     <div class="flex justify-center">
       <img src="/images/playlists/green-tea-bag.svg" alt="Green Tea Bag Icon" />
@@ -11,58 +11,31 @@
         Hand cutting dovetails can be a slow & therapudic thing. It allows your mind to slow down
         and think a little more deeply than it normally would.
       </p>
-      <p class="py-3">
+      <p class="py-3 mb-5">
         This playlist was created in a low point in my life, which is reflected in many of the
         song's themes. It is a series of songs that tend to lead my mind into introspection which
         helped me process a lot of my emotions and thoughts in a healthy way.
       </p>
       <a
-        href="#"
+        href="https://open.spotify.com/playlist/7Bt8uzRoIt0rfMbysXaCQs?si=e401475b77ee4d9f"
+        target="_blank"
         class="
           uppercase
-          px-4
-          py-1
-          rounded-2xl
+          px-5
+          py-2
+          rounded-full
+          text-md
+          font-bold
+          text-green-400
           bg-green-50
-          text-green-700
-          text-sm
-          font-bold"
-      >
-        Listen Now
-      </a>
-    </div>
-  </div>
-</div>
-
-<div class="px-7 my-7 lg:flex md:px-20 mx-auto max-w-7xl">
-  <div
-    class="bg-yellow-200 p-8 grid grid-cols-[275px_minmax(0px,_1fr)_0px] items-center selection:bg-yellow-50 selection:text-yellow-800"
-  >
-    <div class="flex justify-center">
-      <img src="/images/playlists/people-together.svg" alt="Green Tea Bag Icon" />
-    </div>
-    <div class="text-yellow-700 text-base">
-      <h2 class="font-semibold text-4xl text-yellow-900">People Over</h2>
-      <p class="pt-3">
-        Hand cutting dovetails can be a slow & therapudic thing. It allows your mind to slow down
-        and think a little more deeply than it normally would.
-      </p>
-      <p class="py-3">
-        This playlist was created in a low point in my life, which is reflected in many of the
-        song's themes. It is a series of songs that tend to lead my mind into introspection which
-        helped me process a lot of my emotions and thoughts in a healthy way.
-      </p>
-      <a
-        href="#"
-        class="
-          uppercase
-          px-4
-          py-1
-          rounded-2xl
-          bg-yellow-50
-          text-yellow-700
-          text-sm
-          font-bold"
+          border-4
+          border-green-200
+          hover:shadow
+          shadow-green-800
+          hover:ring-2
+          ring-green-400
+          transition-all
+          "
       >
         Listen Now
       </a>
