5b368d9cd6b7fac7c5030d1b712cd4f95dc72ce4
Jordan Calhoun <jordancalhoun@gmail.com>
2022-10-11 23:09:42 -0400

    add most basic implementation of form emailing

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,15 +1,13 @@
 import type { Actions } from './$types';
 import { invalid } from '@sveltejs/kit';
+import { sendEmail } from '$lib/utils/sendEmail';
 
 export const actions: Actions = {
-  default: async ({ cookies, request }) => {
-    //TODO: Send the contact email
-    // console.log('Hello from the action page!');
-    // let response = await event.request.formData();
-    // console.log(response.get('email'));
+  default: async ({ request }) => {
     const data = await request.formData();
     const email = data.get('email');
-    const name = data.get('name');
+    const name = data.get('name') ? data.get('name') : 'Unknown Sender';
+    const message = data.get('message');
 
     if (!email) {
       // Invalid function generates a return depending on the arguments passed,
@@ -17,10 +15,25 @@ export const actions: Actions = {
       return invalid(400, { email, missingEmail: true });
     }
 
+    if (!message) {
+      return invalid(400, { message, missingMessage: true });
+    }
+
+    if (name && email && message) {
+      const emailSendStatus = await sendEmail(
+        name.toString(),
+        email.toString(),
+        message.toString()
+      );
+      return {
+        success: emailSendStatus,
+        name: name,
+        email: email,
+      };
+    }
+
     return {
-      success: true,
-      name: name,
-      email: email,
+      success: false,
     };
   },
 };
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   import { enhance } from '$app/forms';
   import type { ActionData } from './$types';
-
   export let form: ActionData;
 </script>
 
 {#if form?.success}
   <p>Thank you for your email, {form.name}</p>
+{:else if form?.success === false}
+  <p>Email sending error</p>
 {:else}
   <h1>Contact me</h1>
   <form method="post" use:enhance>
@@ -16,6 +17,8 @@
       <p>Please enter your email address</p>
     {/if}
     <input type="email" name="email" id="email" placeholder="Email address" required />
+    <br />
+    <textarea name="message" cols="30" rows="10" />
     <br />
     <button>Submit</button>
   </form>
