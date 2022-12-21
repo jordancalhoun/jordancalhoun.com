2ce181efe3872524a2b198aa90cdc29e2365c90e
Jordan Calhoun <jordancalhoun@gmail.com>
2022-12-21 14:57:08 -0500

    Add Mood Playlist

--- src/routes/playlists/+page.svelte
+++ src/routes/playlists/+page.svelte
@@ -22,6 +22,30 @@
       </p>
 
       <a
+        href="https://open.spotify.com/playlist/7Bt8uzRoIt0rfMbysXaCQs?si=e401475b77ee4d9f"
+        target="_blank"
+        class="
+          uppercase
+          px-7
+          py-2
+          rounded-full
+          text-md
+          font-bold
+          text-green-400
+          bg-green-50
+          border-4
+          border-green-200
+          hover:shadow
+          shadow-green-800
+          hover:ring-2
+          ring-green-400
+          transition-all
+          "
+      >
+        Spotify
+      </a>
+
+      <a
         href="https://tidal.com/browse/playlist/f915f9be-4e5f-4c04-844a-342352fc208f"
         target="_blank"
         class="
@@ -44,9 +68,27 @@
       >
         Tidal
       </a>
+    </div>
+  </div>
+</div>
+
+<div class="px-2 md:px-7 my-2 md:my-7 lg:flex  mx-auto max-w-7xl">
+  <div
+    class="bg-yellow-200 p-3 md:p-8 md:pl-0 md:grid md:grid-cols-[275px_minmax(0px,_1fr)_0px] items-center selection:bg-yellow-100 selection:text-yellow-900 rounded-md"
+  >
+    <div class="flex justify-center p-5 md:p-0">
+      <img src="/images/playlists/mood-emoji.svg" alt="Apathetic Emoji" />
+    </div>
+    <div class="text-yellow-700 text-base pb-5 p-3 md:p-0">
+      <h2 class="font-semibold text-4xl text-yellow-900">Mood</h2>
+      <p class="pt-3 mb-5">
+        Sometimes you’re just feeling a certain kinda way. Not quite sad, nor angry. Apathetic
+        maybe? This playlist is for those moods where all you need is a set of headphones and a
+        comfy chair.
+      </p>
 
       <a
-        href="https://open.spotify.com/playlist/7Bt8uzRoIt0rfMbysXaCQs?si=e401475b77ee4d9f"
+        href="https://open.spotify.com/playlist/6W4EqoTMa0dxzcHWBukVhu?si=83a3a67a9a674d36"
         target="_blank"
         class="
           uppercase
@@ -55,14 +97,14 @@
           rounded-full
           text-md
           font-bold
-          text-green-400
-          bg-green-50
+          text-yellow-700
+          bg-yellow-50
           border-4
-          border-green-200
+          border-yellow-100
           hover:shadow
-          shadow-green-800
+          shadow-yellow-800
           hover:ring-2
-          ring-green-400
+          ring-yellow-400
           transition-all
           "
       >
