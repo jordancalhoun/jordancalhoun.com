e879440767e4108a19b7fc09e410f9261e55443c
Jordan Calhoun <jordancalhoun@gmail.com>
2023-03-12 15:17:56 -0400

    Add newsletter to bottom of blog post

--- src/routes/blog/[post]/+page.svelte
+++ src/routes/blog/[post]/+page.svelte
@@ -1,75 +1,76 @@
 <script lang="ts">
-  import CoverPhotoCredit from './CoverPhotoCredit.svelte';
+	import CoverPhotoCredit from './CoverPhotoCredit.svelte';
 
-  import { generateFriendlyDate } from '$lib/utils/date';
-  import type { PageData } from './$types';
+	import { generateFriendlyDate } from '$lib/utils/date';
+	import type { PageData } from './$types';
+	import Newsletter from '$lib/components/Newsletter.svelte';
 
-  export let data: PageData;
+	export let data: PageData;
 
-  const {
-    title,
-    date,
-    updated,
-    categories,
-    coverImage,
-    coverWidth,
-    coverHeight,
-    excerpt,
-    coverPhotoCredited,
-  } = data.meta;
+	const {
+		title,
+		date,
+		updated,
+		categories,
+		coverImage,
+		coverWidth,
+		coverHeight,
+		excerpt,
+		coverPhotoCredited
+	} = data.meta;
 
-  const { coverAuthor, coverAuthorSource, coverSource } = data.meta;
+	const { coverAuthor, coverAuthorSource, coverSource } = data.meta;
 
-  const friendlyDate = generateFriendlyDate(date);
-  const friendlyUpdate = updated && generateFriendlyDate(updated);
+	const friendlyDate = generateFriendlyDate(date);
+	const friendlyUpdate = updated && generateFriendlyDate(updated);
 </script>
 
 <svelte:head>
-  <title>{title} by Jordan Calhoun</title>
-  <meta data-key="description" name="description" content={excerpt} />
-  <meta property="og:type" content="article" />
-  <meta property="og:title" content={title} />
-  <meta name="twitter:title" content={title} />
-  <meta property="og:description" content={excerpt} />
-  <meta name="twitter:description" content={excerpt} />
-  <meta property="og:image" content="https://jordancalhoun.com/images/blog/{coverImage}" />
-  <meta property="og:image:width" content={coverWidth} />
-  <meta property="og:image:height" content={coverHeight} />
-  <meta name="twitter:image" content="https://jordancalhoun.com/images/blog/{coverImage}" />
+	<title>{title} by Jordan Calhoun</title>
+	<meta data-key="description" name="description" content={excerpt} />
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={title} />
+	<meta name="twitter:title" content={title} />
+	<meta property="og:description" content={excerpt} />
+	<meta name="twitter:description" content={excerpt} />
+	<meta property="og:image" content="https://jordancalhoun.com/images/blog/{coverImage}" />
+	<meta property="og:image:width" content={coverWidth} />
+	<meta property="og:image:height" content={coverHeight} />
+	<meta name="twitter:image" content="https://jordancalhoun.com/images/blog/{coverImage}" />
 </svelte:head>
 
 <!-- TODO: Implement Share Actions -->
 <article class="mx-auto max-w-4xl md:my-10 md:px-10 prose prose-neutral">
-  <img
-    src="/images/blog/{coverImage}"
-    alt=""
-    style="aspect-ratio: {coverWidth} / {coverHeight};"
-    class="
+	<img
+		src="/images/blog/{coverImage}"
+		alt=""
+		style="aspect-ratio: {coverWidth} / {coverHeight};"
+		class="
       shadow-md
       md:hidden
       mb-0
     "
-  />
-  <div class="md:hidden mb-5 pl-1">
-    {#if coverPhotoCredited}
-      <CoverPhotoCredit
-        author={coverAuthor}
-        authorSource={coverAuthorSource}
-        source={coverSource}
-      />
-    {/if}
-  </div>
+	/>
+	<div class="md:hidden mb-5 pl-1">
+		{#if coverPhotoCredited}
+			<CoverPhotoCredit
+				author={coverAuthor}
+				authorSource={coverAuthorSource}
+				source={coverSource}
+			/>
+		{/if}
+	</div>
 
-  <h1 class="text-center text-neutral-800 font-semibold text-4xl mb-5 p-2">{title}</h1>
-  <p class="text-center">
-    <span class="text-sm text-neutral-400">Posted: {friendlyDate}</span>
-    {#if friendlyUpdate}
-      <span class="text-sm text-neutral-400"> - Last updated: {friendlyUpdate}</span>
-    {/if}
-    {#each categories as category}
-      <a
-        href="/blog/category/{category}"
-        class="
+	<h1 class="text-center text-neutral-800 font-semibold text-4xl mb-5 p-2">{title}</h1>
+	<p class="text-center">
+		<span class="text-sm text-neutral-400">Posted: {friendlyDate}</span>
+		{#if friendlyUpdate}
+			<span class="text-sm text-neutral-400"> - Last updated: {friendlyUpdate}</span>
+		{/if}
+		{#each categories as category}
+			<a
+				href="/blog/category/{category}"
+				class="
               px-2
               py-1
               mx-2
@@ -81,16 +82,16 @@
               font-bold
               hover:bg-neutral-300
               hover:text-neutral-700"
-      >
-        {category.toUpperCase()}
-      </a>
-    {/each}
-  </p>
-  <img
-    src="/images/blog/{coverImage}"
-    alt=""
-    style="aspect-ratio: {coverWidth} / {coverHeight};"
-    class="
+			>
+				{category.toUpperCase()}
+			</a>
+		{/each}
+	</p>
+	<img
+		src="/images/blog/{coverImage}"
+		alt=""
+		style="aspect-ratio: {coverWidth} / {coverHeight};"
+		class="
       mt-9
       md:rounded-lg
       shadow-md
@@ -100,19 +101,19 @@
       border-neutral-100
       mb-0
     "
-  />
-  <div class="hidden md:block">
-    {#if coverPhotoCredited}
-      <CoverPhotoCredit
-        author={coverAuthor}
-        authorSource={coverAuthorSource}
-        source={coverSource}
-      />
-    {/if}
-  </div>
+	/>
+	<div class="hidden md:block">
+		{#if coverPhotoCredited}
+			<CoverPhotoCredit
+				author={coverAuthor}
+				authorSource={coverAuthorSource}
+				source={coverSource}
+			/>
+		{/if}
+	</div>
 
-  <div
-    class="
+	<div
+		class="
     px-7
     md:px-0
     prose-img:rounded-lg
@@ -123,7 +124,19 @@
     first-letter:mr-3
     first-letter:float-left
     first-line:font-serif"
-  >
-    {@html data.PostContent}
-  </div>
+	>
+		{@html data.PostContent}
+	</div>
+	<div class="md:p-10 mx-auto max-w-4xl">
+		<h3 class="text-neutral-800 text-lg font-semibold mt-5 mx-auto text-center">
+			Join the Newsletter
+		</h3>
+		<div class="mx-auto">
+			<script
+				async
+				data-uid="7518f1ca76"
+				src="https://exciting-author-7209.ck.page/7518f1ca76/index.js"
+			></script>
+		</div>
+	</div>
 </article>
--- src/routes/playlists/+page.svelte
+++ src/routes/playlists/+page.svelte
@@ -1,31 +1,31 @@
 <svelte:head>
-  <title>Playlists created by Jordan Calhoun</title>
+	<title>Playlists created by Jordan Calhoun</title>
 </svelte:head>
 
 <div class="px-2 md:px-7 my-2 md:my-7 lg:flex  mx-auto max-w-7xl">
-  <div
-    class="bg-green-300 p-3 md:p-8 md:pl-0 md:grid md:grid-cols-[275px_minmax(0px,_1fr)_0px] items-center selection:bg-green-100 selection:text-green-900 rounded-md"
-  >
-    <div class="flex justify-center p-5 md:p-0">
-      <img src="/images/playlists/green-tea-bag.svg" alt="Green Tea Bag Icon" />
-    </div>
-    <div class="text-green-700 text-base pb-5 p-3 md:p-0">
-      <h2 class="font-semibold text-4xl text-green-900">Dovetails &amp; Green Tea</h2>
-      <p class="pt-3">
-        Hand cutting dovetails can be a slow & therapeutic thing. It allows your mind to slow down
-        and to think a little more deeply than it normally might.
-      </p>
-      <p class="py-3 mb-5">
-        This playlist was created at a low point in my life, which is reflected in many of the
-        songs' themes. It is a series of songs that tend to lead my mind into introspection which
-        helped me process a lot of my emotions and thoughts in a healthy way.
-      </p>
+	<div
+		class="bg-green-300 p-3 md:p-8 md:pl-0 md:grid md:grid-cols-[275px_minmax(0px,_1fr)_0px] items-center selection:bg-green-100 selection:text-green-900 rounded-md"
+	>
+		<div class="flex justify-center p-5 md:p-0">
+			<img src="/images/playlists/green-tea-bag.svg" alt="Green Tea Bag Icon" />
+		</div>
+		<div class="text-green-700 text-base pb-5 p-3 md:p-0">
+			<h2 class="font-semibold text-4xl text-green-900">Dovetails &amp; Green Tea</h2>
+			<p class="pt-3">
+				Hand cutting dovetails can be a slow & therapeutic thing. It allows your mind to slow down
+				and to think a little more deeply than it normally might.
+			</p>
+			<p class="py-3 mb-5">
+				This playlist was created at a low point in my life, which is reflected in many of the
+				songs' themes. It is a series of songs that tend to lead my mind into introspection which
+				helped me process a lot of my emotions and thoughts in a healthy way.
+			</p>
 
-      <a
-        href="https://open.spotify.com/playlist/7Bt8uzRoIt0rfMbysXaCQs?si=e401475b77ee4d9f"
-        target="_blank"
-        rel="noreferrer"
-        class="
+			<a
+				href="https://open.spotify.com/playlist/7Bt8uzRoIt0rfMbysXaCQs?si=e401475b77ee4d9f"
+				target="_blank"
+				rel="noreferrer"
+				class="
           uppercase
           px-7
           py-2
@@ -42,15 +42,15 @@
           ring-green-400
           transition-all
           "
-      >
-        Spotify
-      </a>
+			>
+				Spotify
+			</a>
 
-      <a
-        href="https://tidal.com/browse/playlist/f915f9be-4e5f-4c04-844a-342352fc208f"
-        target="_blank"
-        rel="noreferrer"
-        class="
+			<a
+				href="https://tidal.com/browse/playlist/f915f9be-4e5f-4c04-844a-342352fc208f"
+				target="_blank"
+				rel="noreferrer"
+				class="
           uppercase
           px-7
           py-2
@@ -67,33 +67,33 @@
           ring-green-400
           transition-all
           "
-      >
-        Tidal
-      </a>
-    </div>
-  </div>
+			>
+				Tidal
+			</a>
+		</div>
+	</div>
 </div>
 
 <div class="px-2 md:px-7 my-2 md:my-7 lg:flex  mx-auto max-w-7xl">
-  <div
-    class="bg-yellow-200 p-3 md:p-8 md:pl-0 md:grid md:grid-cols-[275px_minmax(0px,_1fr)_0px] items-center selection:bg-yellow-100 selection:text-yellow-900 rounded-md"
-  >
-    <div class="flex justify-center p-5 md:p-0">
-      <img src="/images/playlists/mood-emoji.svg" alt="Apathetic Emoji" />
-    </div>
-    <div class="text-yellow-700 text-base pb-5 p-3 md:p-0">
-      <h2 class="font-semibold text-4xl text-yellow-900">Mood</h2>
-      <p class="pt-3 mb-5">
-        Sometimes you're just feeling a certain kinda way. Not quite sad, nor angry. Apathetic
-        maybe? This playlist is for those moods when all you need is a set of headphones and a comfy
-        chair.
-      </p>
+	<div
+		class="bg-yellow-200 p-3 md:p-8 md:pl-0 md:grid md:grid-cols-[275px_minmax(0px,_1fr)_0px] items-center selection:bg-yellow-100 selection:text-yellow-900 rounded-md"
+	>
+		<div class="flex justify-center p-5 md:p-0">
+			<img src="/images/playlists/mood-emoji.svg" alt="Apathetic Emoji" />
+		</div>
+		<div class="text-yellow-700 text-base pb-5 p-3 md:p-0">
+			<h2 class="font-semibold text-4xl text-yellow-900">Mood</h2>
+			<p class="pt-3 mb-5">
+				Sometimes you're just feeling a certain kinda way. Not quite sad, nor angry. Apathetic
+				maybe? This playlist is for those moods when all you need is a set of headphones and a comfy
+				chair.
+			</p>
 
-      <a
-        href="https://open.spotify.com/playlist/6W4EqoTMa0dxzcHWBukVhu?si=83a3a67a9a674d36"
-        target="_blank"
-        rel="noreferrer"
-        class="
+			<a
+				href="https://open.spotify.com/playlist/6W4EqoTMa0dxzcHWBukVhu?si=83a3a67a9a674d36"
+				target="_blank"
+				rel="noreferrer"
+				class="
           uppercase
           px-7
           py-2
@@ -110,9 +110,9 @@
           ring-yellow-400
           transition-all
           "
-      >
-        Spotify
-      </a>
-    </div>
-  </div>
+			>
+				Spotify
+			</a>
+		</div>
+	</div>
 </div>
